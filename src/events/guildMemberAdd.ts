e795ac08c8de4566d17b37539c261cfbac0a3e40
root <root@rl.supply>
2020-07-26 20:59:07 -0700

    Normalised name, optimised data storage + bans

--- src/events/guildMemberAdd.ts
+++ src/events/guildMemberAdd.ts
@@ -1,15 +1,18 @@
 import { client } from "..";
 import filter from "../filter";
 import { ban } from "../methods/ban";
-import { GuildMember } from "discord.js";
+import { GuildMember, Collection } from "discord.js";
+
+type MemberJoinEntry = {
+    guild: string;
+    joined: number;
+    banned: boolean;
+}
 
 let nameLog: {
     [index: string]: {
-        id: string;
-        guild: string;
-        joined: number;
-        banned: boolean;
-    }[];
+        [index: string]: Collection<string, MemberJoinEntry>
+    };
 } = {};
 
 const hour = 1000 * 60 * 60;
@@ -21,25 +24,24 @@ client.on("guildMemberAdd", async (member) => {
     for (let test of filter) {
         if (name.search(test) !== -1) return ban(member, `Filter violation: \`${test}\``);
     }
-    //Multiple Same Name Accounts
-    nameLog[member.displayName] = nameLog[member.displayName] || [];
-    if (!nameLog[member.displayName].find((m) => m.id === member.id))
-        nameLog[member.displayName].push({
-            id: member.id,
-            guild: member.guild.id,
-            joined: Date.now(),
-            banned: false
-        });
+    // *** Multiple Same Name Accounts ***
+    //Initialise Guild + Name Collections
+    if (!nameLog[member.guild.id]) nameLog[member.guild.id] = {}
+    if (!nameLog[member.guild.id][member.id]) nameLog[member.guild.id][member.id] = new Collection();
+    //Add Member Record
+    let members = nameLog[member.guild.id][member.id]
+    members.set(member.id, {
+        guild: member.guild.id,
+        joined: Date.now(),
+        banned: false
+    });
     //If Account w/ Same Name in Last Hour
-    let members = nameLog[member.displayName];
-    if (members.length > 1 && members[members.length - 2].joined > Date.now() - hour) {
-        //Ban All Recent Accounts with Same Name
-        for (let i in members) {
-            let entry = members[i];
-            if (!entry.banned && entry.joined > Date.now() - hour) {
-                await ban(await client.guilds.resolve(entry.guild).members.fetch(entry.id), "Duplicate username");
-                entry.banned = true;
-            }
+    if (members.size > 1 && members.array()[members.size - 2].joined > Date.now() - hour) {
+        //Ban Recent
+        let membersToBan = [members.keyArray()[members.size - 2], members.keyArray()[members.size - 1]]
+        for (let id of membersToBan) {
+            await ban(await member.guild.members.fetch(id), "Duplicate username");
+            members.set(id, { ...members.get(id), banned: true });
         }
     }
 });
